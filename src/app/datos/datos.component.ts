ad6b6d627a351106ca786bad0bb6ab795ff67327
tavitos <lesamaoyabi@gmail.com>
2024-02-15 18:14:54 -0600

    Desplegado de NT y NA y datos del usuario.

--- src/app/datos/datos.component.ts
+++ src/app/datos/datos.component.ts
@@ -22,6 +22,19 @@ export class DatosComponent implements OnInit{
 
   public sum_order!: number;
 
+  public id_client_bp!: string;
+  public rfc!: string;
+  public gender!: string;
+  public phone_1!: string;
+  public phone_2!: string;
+  public cellphone!: string;
+  public email_1!: string;
+  public city!: string;
+  public delegacy!: string;
+  public colony!: string;
+  public address!: string;
+  public zip_code!: string;
+
   @ViewChild(MatSort) sort!: MatSort;
 
   constructor(
@@ -31,6 +44,19 @@ export class DatosComponent implements OnInit{
 
   ngOnInit(): void {
     this.fullName = this.data.full_name;
+    this.id_client_bp = this.data.id_client_bp;
+    this.rfc = this.data.rfc;
+    this.gender = this.data.gender;
+    this.phone_1 = this.data.phone_1;
+    this.phone_2 = this.data.phone_2;
+    this.cellphone = this.data.cellphone;
+    this.email_1 = this.data.email_1;
+    this.city = this.data.city;
+    this.delegacy = this.data.delegacy;
+    this.colony = this.data.colony;
+    this.address = this.data.address;
+    this.zip_code = this.data.zip_code;
+    
     console.log(this.data.id);
     this.getVehiclesWithOrdersByCustomer(this.data.id);
     this.sum_order = this.data.sum_orders;
