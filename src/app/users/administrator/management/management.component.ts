8bb6b37af76ada1477886a4ba4b1e9664045f9c4
Stefan <sreyes@codecreativity.mx>
2024-03-11 09:28:20 -0600

    graficas test

---
+++ src/app/users/administrator/management/management.component.ts
@@ -0,0 +1,145 @@
+import { Component, OnInit } from '@angular/core';
+import * as echarts from 'echarts';
+@Component({
+  selector: 'app-management',
+  templateUrl: './management.component.html',
+  styleUrls: ['./management.component.css']
+})
+export class ManagementComponent implements OnInit {
+  echartsInstance?: echarts.ECharts;
+
+  ngOnInit(): void {
+    this.initDoughnutChart();
+    this.initPieChart();
+    this.stackedLineChart();
+  }
+
+  initDoughnutChart(): void {
+    this.echartsInstance = echarts.init(document.getElementById('doughnut-chart') as HTMLDivElement);
+    this.echartsInstance.setOption({
+      title: {
+        text: 'Referer of a Website',
+        subtext: 'Fake Data',
+        left: 'center'
+      },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left'
+      },
+      series: [
+        {
+          name: 'Access From',
+          type: 'pie',
+          radius: '50%',
+          data: [
+            { value: 1048, name: 'Search Engine' },
+            { value: 735, name: 'Direct' },
+            { value: 580, name: 'Email' },
+            { value: 484, name: 'Union Ads' },
+            { value: 300, name: 'Video Ads' }
+          ],
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }
+      ]
+    });
+  }
+
+  initPieChart(): void {    
+    this.echartsInstance = echarts.init(document.getElementById('pie-chart') as HTMLDivElement);
+    this.echartsInstance.setOption({      
+        tooltip: {
+          trigger: 'item'
+        },
+        legend: {
+          top: '5%',
+          left: 'center'
+        },
+        series: [
+          {
+            name: 'Access From',
+            type: 'pie',
+            radius: ['40%', '70%'],
+            avoidLabelOverlap: false,
+            label: {
+              show: false,
+              position: 'center'
+            },
+            emphasis: {
+              label: {
+                show: true,
+                fontSize: 40,
+                fontWeight: 'bold'
+              }
+            },
+            labelLine: {
+              show: false
+            },
+            data: [
+              { value: 1048, name: 'Search Engine' },
+              { value: 735, name: 'Direct' },
+              { value: 580, name: 'Email' },
+              { value: 484, name: 'Union Ads' },
+              { value: 300, name: 'Video Ads' }
+            ]
+          }
+        ]      
+    });
+  }
+
+  stackedLineChart(): void {
+    this.echartsInstance = echarts.init(document.getElementById('stacked-line') as HTMLDivElement);
+    this.echartsInstance.setOption({      
+      title: {
+        text: 'Comparación semana anterior'
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      legend: {
+        data: ['Actual', 'Pasada']
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {}
+        }
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          name: 'Actual',
+          type: 'line',
+          stack: 'Total',
+          data: [320, 332, 301, 334, 390, 330, 320]
+        },
+        {
+          name: 'Pasada',
+          type: 'line',
+          stack: 'Total',
+          data: [820, 932, 901, 934, 1290, 1330, 1320]
+        }
+      ]
+    });
+  }
+}
