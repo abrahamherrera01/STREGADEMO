ea13fb080f1f7077ed384b754394998a5b2d800b
Stefan <sreyes@codecreativity.mx>
2024-03-19 12:21:12 -0600

    responsive tablas

--- src/app/users/administrator/manage-managers/manage-managers.component.ts
+++ src/app/users/administrator/manage-managers/manage-managers.component.ts
@@ -1,7 +1,27 @@
 import { Component, ElementRef } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AddManagerComponent } from '../components/add-manager/add-manager.component';
+import Swal from "sweetalert2";
+export interface Lead {
+  name: string;
+  age: number;  
+  email: string;
+}
 
+const ELEMENT_DATA1: Lead[] = [
+  { name: 'Hydrogen', age: 12, email: 'H282@gmail.com'},
+  { name: 'Helium', age: 42, email: 'He282@gmail.com'},
+  { name: 'Lithium', age: 62, email: 'Li282@gmail.com'},
+  { name: 'Beryllium', age: 92, email: 'Be282@gmail.com'},
+  { name: 'Boron', age: 12, email: 'B282@gmail.com'},  
+];
+const ELEMENT_DATA2: Lead[] = [  
+  { name: 'Carbon', age: 12, email: 'C282@gmail.com'},
+  { name: 'Nitrogen', age: 17, email: 'N282@gmail.com'},
+  { name: 'Oxygen', age: 14, email: 'O282@gmail.com'},
+  { name: 'Fluorine', age: 14, email: 'F282@gmail.com'},
+  { name: 'Neon', age: 22, email: 'Ne282@gmail.com'},
+];
 @Component({
   selector: 'app-manage-managers',
   templateUrl: './manage-managers.component.html',
@@ -13,23 +33,47 @@ export class ManageManagersComponent {
     { text: 'Administrar gestores' }
   ];
 
+  displayedColumns: string[] = ['add', 'nombre', 'edad', 'email'];
+  managersToBeAssigned: string[] = ['add', 'nombre', 'edad', 'email', 'actualizar', 'revisar', 'eliminar'];
+  dataSource1 = ELEMENT_DATA1;
+  dataSource2 = ELEMENT_DATA2;
+
   constructor(
     private elementRef: ElementRef,
     private dialog: MatDialog
   ) { }
 
-  addManager() {
-    // animación al dar click 
-    const button = this.elementRef.nativeElement.querySelector('.animate__rubberBand');
-    button.classList.remove('animate__rubberBand');
-    void button.offsetWidth;
-    button.classList.add('animate__rubberBand');
-    // fin animación al dar click    
+  addManagerOrUpdate( manager_id:number|null = null ) {
+    if( manager_id === null ){
+      // animación al dar click 
+      const button = this.elementRef.nativeElement.querySelector('.add_manager');
+      button.classList.remove('animate__rubberBand');
+      void button.offsetWidth;
+      button.classList.add('animate__rubberBand');
+      // fin animación al dar click    
+    }    
 
-    const dialogRef = this.dialog.open(AddManagerComponent);
+    const dialogRef = this.dialog.open(AddManagerComponent, {
+      data: { manager_id:manager_id }
+    });
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('El modal ha sido cerrado');
+    });
+  }
+
+  deleteManager() {
+    Swal.fire({
+      title: "¿Está seguro de que quieres eliminar este registro?",      
+      showCancelButton: true,
+      confirmButtonText: "Eliminar",      
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire("Registro eliminado!", "", "success");
+      } else if (result.isDenied) {
+        Swal.fire("Ocurrió un error", "", "info");
+      }
     });
   }
 }
