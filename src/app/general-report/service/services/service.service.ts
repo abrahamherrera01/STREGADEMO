88be0d19e777120f52e7daa74515822e3ab23f7b
Abraham Herrera <f170004@gmail.com>
2024-05-03 09:14:11 -0600

    cambios

--- src/app/general-report/service/services/service.service.ts
+++ src/app/general-report/service/services/service.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { DashboardVentas } from '../interfaces/dashboard-ventas.interface';
 import { UntraceableTypeExecutive } from '../interfaces/untraceable-type-executive';
+import { getQuarterlyComparisonInterface } from '../interfaces/get-quarterly-comparison.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -24,4 +25,7 @@ export class ServiceService {
     return this._http.get<UntraceableTypeExecutive>(`${this.url}/api/untraceableTypeExecutive`);
   }
 
+  getQuarterlyComparison():Observable<getQuarterlyComparisonInterface>{
+    return this._http.get<getQuarterlyComparisonInterface>(`${this.url}/api/quarterlyComparisons`);
+  }
 }
